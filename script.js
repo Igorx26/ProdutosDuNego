f589157c8370a19a35513a2d9d1d345fb34bace2
Igor Martins Silva <igor12-04@hotmail.com>
2024-06-01 19:07:45 -0300

    product update

--- script.js
+++ script.js
@@ -222,7 +222,7 @@ checkoutBtn.addEventListener("click", function () {
 function checkRestaurantOpen() {
   const data = new Date();
   const hour = data.getHours();
-  return hour >= 9 && hour <= 16;
+  return hour >= 9 && hour <= 22;
 }
 
 const spanItem = document.getElementById("date-span");
